1101853e2b63a8311b678e2ff4155c18e19283ee
Akash K K <Akash.KK@ibm.com>
2024-09-07 11:58:11 +0530

    Remove unused imports from EmailJob.java
    Clean up code by removing unnecessary Jakarta Mail and MimeMessage imports. This improves code readability and eliminates potential warnings from unused imports.

--- src/main/java/com/app/springbootquartzschedulerapplication/quartz/job/EmailJob.java
+++ src/main/java/com/app/springbootquartzschedulerapplication/quartz/job/EmailJob.java
@@ -1,7 +1,5 @@
 package com.app.springbootquartzschedulerapplication.quartz.job;
 
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
@@ -9,11 +7,8 @@ import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.mail.MailProperties;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 import org.springframework.stereotype.Component;
-
-import java.nio.charset.StandardCharsets;
 
 @Component
 @Slf4j
